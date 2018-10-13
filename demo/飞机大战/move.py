df58e07dd9f8bf8317df56f689c97a8fd7854bea
longjiawei <309842681@qq.com>
2018-10-13 12:19:29 +0800

    更新

--- demo/飞机大战/move.py
+++ demo/飞机大战/move.py
@@ -4,14 +4,46 @@ import sys
 import pygame
 
 
+class Enemyplane():
+    def __init__(self):
+        self.enemyplane = pygame.image.load('D:\python\demo\飞机大战\图片\\14.1.png')
+        self.enemyplane1 = pygame.image.load('D:\python\demo\飞机大战\图片\\14.1.png')
+        self.enemyplaneRect = pygame.Rect(60,60,60,60)
+        self.enemyplaneRect1 = pygame.Rect(60, 60, 60, 60)
+        self.enemyplanex = 200
+        self.enemyplanex1 = 400
+        self.enemplaney = 200
+        self.enemyplaney1 = 200
+        self.enemyplaneRect[1] = self.enemplaney
+        self.enemyplaneRect1[1] = self.enemyplaney1
+    def enemymove(self):
+        pass
 class Bullet():
-    pass
+    def __init__(self):
+
+        self.bullet = pygame.image.load('D:\python\demo\飞机大战\图片\\15.1.png')
+        self.bulletrect = pygame.Rect(30,30,30,30)
+        self.bulletx = Heroplane.planex + 10
+        self.bullety = Heroplane.planey - 20
+        self.bulletspeed = 2
+    def bulletmove(self):
+        self.bullety -= self.bulletspeed
+        if self.bullety < 0:
+            self.bullety = Heroplane.planey - 20
+            self.bulletx = Heroplane.planex + 10
+        self.bulletrect[1] = self.bullety
+        self.bulletrect[0] = self.bulletx
+
 class Heroplane():
     def __init__(self):
-        self.plane = pygame.image.load('D:\python\demo\飞机大战\图片\\1.2.png')
+        self.plane = pygame.image.load('D:\python\demo\飞机大战\图片\\1.3.png')
         self.planeRect = pygame.Rect(50,50,50,50)
         self.planex = 275
         self.planey = 740
+        # self.herobullets = [Bullet()for _ in range(1,30)]
+        # for self.herobuttet in self.herobullets:
+        #     self.herobullets.append(self.herobuttet)
+
     def planemove(self):
         if event.key == pygame.K_UP:
             self.planey -= 5
@@ -28,22 +60,27 @@ class Heroplane():
 
 class Long():
     def __init__(self):
-        self.feifei = pygame.image.load('D:\python\demo\飞机大战\图片\\10.2.png')
+        self.feifei = pygame.image.load('D:\python\demo\飞机大战\图片\\10.3.png')
         self.feifeiRect = pygame.Rect(65,50,65,50)
         self.feifeix = random.randint(100,500)
         self.feifeiy = -50
-
-
+        self.feifei1 = pygame.image.load('D:\python\demo\飞机大战\图片\\10.3.png')
+        self.feifei1Rect = pygame.Rect(65, 50, 65, 50)
+        self.feifei1x = random.randint(100, 500)
+        self.feifei1y = -50
     def updateBee(self):
-        self.feifeiy += 5
+        self.feifeiy += 4
         self.feifeiRect[1] = self.feifeiy
         if self.feifeiy > 800 :
             self.feifeiy = 0
             self.feifeix = random.randint(100,500)
-
+        self.feifei1y += 3
+        self.feifei1Rect[1] = self.feifei1y
+        if self.feifei1y > 800:
+            self.feifei1y = 0
+            self.feifei1x = random.randint(100, 500)
 class Map():
     def __init__(self):
-
         self.background1 = pygame.image.load('D:\python\demo\飞机大战\图片\\2.1.png')
         self.background2 = pygame.image.load('D:\python\demo\飞机大战\图片\\2.1.png')
         self.bg1rect = pygame.Rect(0,0,600,900)
@@ -66,8 +103,13 @@ def Display():
     screen.fill((255, 255, 255))
     screen.blit(Map.background1, (0, Map.bg1rect[1]))
     screen.blit(Map.background2, (0, Map.bg2rect[1]))
+    screen.blit(Enemyplane.enemyplane,(Enemyplane.enemyplanex,Enemyplane.enemplaney))
+    screen.blit(Enemyplane.enemyplane1,(Enemyplane.enemyplanex1,Enemyplane.enemyplaney1))
     screen.blit(Long.feifei, (Long.feifeix, Long.feifeiRect[1]))
+    screen.blit(Long.feifei1, (Long.feifei1x, Long.feifei1Rect[1]))
     screen.blit(Heroplane.plane, (Heroplane.planex, Heroplane.planey))
+    screen.blit(Bullet.bullet,(Bullet.bulletx,Bullet.bullety))
+    Bullet.bulletmove()
     Map.mapmove()
     Long.updateBee()
     pygame.display.update()
@@ -80,6 +122,8 @@ if __name__ == '__main__':
     Long = Long()
     Heroplane = Heroplane()
     Map = Map()
+    Enemyplane = Enemyplane()
+    Bullet = Bullet()
     clock = pygame.time.Clock()
     while True:
         clock.tick(60)
